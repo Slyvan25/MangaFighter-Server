0d1dc1bbb0657ee23d07913c40a219e4e99ac10a
slyvan25 <silvan.paul123@gmail.com>
2019-12-22 14:45:00 +0100

    Reads the config file

---
+++ MangaFighter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace MangaFighter
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .UseConsoleLifetime()
+                .ConfigureHostConfiguration(config => { config.AddEnvironmentVariables(); })
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("appsettings.json", true, true);
+                    config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+
+                    var configuration = config.Build();
+                    // ServerConfig.Load(configuration);
+                    // Log.Logger = new LoggerConfiguration()
+                    //     .ReadFrom.Configuration(configuration)
+                    //     .Enrich.FromLogContext()
+                    //     .CreateLogger();
+                })
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<ServerManager>(); })
+                .UseSerilog()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
